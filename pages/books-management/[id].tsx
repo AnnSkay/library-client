52a901ad53e4a6d43ce1d503425b06fc19d2c5da
Olga Li <leka.ru97@mail.ru>
2022-01-12 22:07:33 +0300

    edit book

--- pages/books-management/[id].tsx
+++ pages/books-management/[id].tsx
@@ -11,6 +11,7 @@ import cn from "classnames";
 import SearchBookIcon from './icon-search-book.png';
 import EditBookIcon from './icon-edit-book.png';
 import DeleteBookIcon from './icon-delete-book.png';
+import GoBackIcon from './icon-go-back.png';
 import Image from "next/image";
 
 
@@ -23,7 +24,10 @@ export default function BooksManagementPage(): JSX.Element {
   const [books, setBooks] = useState([]);
   const [searchingBookTitle, setSearchingBookTitle] = useState('');
 
-  const [addBookClick, setAddBookClick] = useState(true);
+  const [addBookLinkClick, setAddBookLinkClick] = useState(true);
+  const [editBookLinkClick, setEditBookLinkClick] = useState(false);
+
+  const [searchBookClick, setSearchBookClick] = useState(false);
   const [editBookClick, setEditBookClick] = useState(false);
 
   const [bookValue, setBookValue] = useState({
@@ -67,6 +71,7 @@ export default function BooksManagementPage(): JSX.Element {
         searchingBookTitle
       }).then((response) => {
         setBooks(response.data);
+        setSearchBookClick(true);
       });
   }
 
@@ -92,6 +97,41 @@ export default function BooksManagementPage(): JSX.Element {
       });
   }
 
+  const editBook = (title: string,
+                    author: string,
+                    genreTitle: string,
+                    houseTitle: string,
+                    year: string,
+                    numberCopies: number) => {
+    setBookValue({
+      ...bookValue,
+      title: title,
+      author: author,
+      publishHouse: houseTitle,
+      otherPublishHouse: '',
+      genre: genreTitle,
+      otherGenre: '',
+      publishYear: year,
+      numberCopies: String(numberCopies)
+    });
+  }
+
+  const deleteSelectedBook = async (bookId: number) => {
+    await axios
+      .post('http://localhost:3002/api/deleteBook', {
+        bookId
+      }).then((response) => {
+        alert(response.data);
+        getBooksByTitle();
+      });
+  }
+
+  const deleteBook = (title: string, bookId: number) => {
+    if (confirm(`Вы уверены, что хотите удалить книгу "${title}"?`)) {
+      deleteSelectedBook(bookId);
+    }
+  }
+
   useEffect(() => {
     if (!id) {
       return;
@@ -106,21 +146,227 @@ export default function BooksManagementPage(): JSX.Element {
     }
   };
 
-  const addBookTableClass = () => cn(styles.booksManagementBlock, {
-    [styles.noDisplay]: !addBookClick,
+  const goBack = () => {
+    setAddBookLinkClick(false);
+    setEditBookLinkClick(true);
+    setEditBookClick(false);
+
+    setBookValue({
+      ...bookValue,
+      title: '',
+      author: '',
+      publishHouse: '',
+      otherPublishHouse: '',
+      genre: '',
+      otherGenre: '',
+      publishYear: '',
+      numberCopies: ''
+    });
+  }
+
+  const addBookBlockTableClass = () => cn(styles.booksManagementBlock, {
+    [styles.noDisplay]: !addBookLinkClick,
+  });
+
+  const editBookBlockTableClass = () => cn(styles.booksManagementBlock, {
+    [styles.noDisplay]: !editBookLinkClick,
   });
 
   const editBookTableClass = () => cn(styles.booksManagementBlock, {
     [styles.noDisplay]: !editBookClick,
   });
 
+  const hideManagementBookMenuClass= () => cn({
+    [styles.noDisplay]: editBookClick,
+  });
+
   const activeAddBookClass = () => cn(styles.menuManagementItem, {
-    [styles.activeItem]: addBookClick,
+    [styles.activeItem]: addBookLinkClick,
   });
 
   const activeEditBookClass = () => cn(styles.menuManagementItem, {
-    [styles.activeItem]: editBookClick,
+    [styles.activeItem]: editBookLinkClick,
   });
+
+  const inputFieldsForAddAndEdit = () => {
+    return (
+      <>
+        <tr>
+          <td className={styles.inputTitle}>
+            <label htmlFor="bookTitle">
+              Название книги
+            </label>
+          </td>
+
+          <td>
+            <input
+              type="text"
+              id="bookTitle"
+              name="title"
+              value={bookValue.title}
+              onChange={inputOnChange}
+              className={styles.input}
+            />
+          </td>
+        </tr>
+
+        <tr className={styles.inputBlock}>
+          <td className={styles.inputTitle}>
+            <label htmlFor="bookAuthor">
+              ФИО автора
+            </label>
+          </td>
+
+          <td>
+            <input
+              type="text"
+              id="bookAuthor"
+              name="author"
+              value={bookValue.author}
+              onChange={inputOnChange}
+              className={styles.input}
+            />
+          </td>
+        </tr>
+
+        <tr className={styles.inputBlock}>
+          <td className={styles.inputTitle}>
+            <label htmlFor="bookPublishHouse">
+              Издательство
+            </label>
+          </td>
+
+          <td>
+            <select
+              id="bookPublishHouse"
+              name="publishHouse"
+              value={bookValue.publishHouse}
+              onChange={inputOnChange}
+              className={styles.input}
+            >
+              <option selected />
+              {lists.houses
+              && lists.houses.map(({ title, id }) => {
+                return (
+                  <option key={id + 1}>
+                    {title}
+                  </option>
+                );
+              })}
+            </select>
+          </td>
+        </tr>
+
+        <tr className={styles.inputBlock}>
+          <td className={styles.inputTitle}>
+            <label htmlFor="otherBookPublishHouse">
+              Другое издательство
+            </label>
+          </td>
+
+          <td>
+            <input
+              type="text"
+              id="otherBookPublishHouse"
+              name="otherPublishHouse"
+              value={bookValue.otherPublishHouse}
+              onChange={inputOnChange}
+              className={styles.input}
+            />
+          </td>
+        </tr>
+
+        <tr className={styles.inputBlock}>
+          <td className={styles.inputTitle}>
+            <label htmlFor="bookGenre">
+              Жанр
+            </label>
+          </td>
+
+          <td>
+            <select
+              id="bookGenre"
+              name="genre"
+              value={bookValue.genre}
+              onChange={inputOnChange}
+              className={styles.input}
+            >
+              <option selected />
+              {lists.genres
+              && lists.genres.map(({ title, id }) => {
+                return (
+                  <option key={id + 1}>
+                    {title}
+                  </option>
+                );
+              })}
+            </select>
+          </td>
+        </tr>
+
+        <tr className={styles.inputBlock}>
+          <td className={styles.inputTitle}>
+            <label htmlFor="otherBookGenre">
+              Другой жанр
+            </label>
+          </td>
+
+          <td>
+            <input
+              type="text"
+              id="otherBookGenre"
+              name="otherGenre"
+              value={bookValue.otherGenre}
+              onChange={inputOnChange}
+              className={styles.input}
+            />
+          </td>
+        </tr>
+
+        <tr className={styles.inputBlock}>
+          <td className={styles.inputTitle}>
+            <label htmlFor="bookPublishYear">
+              Год издания
+            </label>
+          </td>
+
+          <td>
+            <input
+              type="number"
+              min="1800"
+              max="2021"
+              id="bookPublishYear"
+              name="publishYear"
+              value={bookValue.publishYear}
+              onChange={inputOnChange}
+              className={cn(styles.input, styles.inputNumber)}
+            />
+          </td>
+        </tr>
+
+        <tr className={styles.inputBlock}>
+          <td className={styles.inputTitle}>
+            <label htmlFor="bookNumberCopies">
+              Количество экземпляров
+            </label>
+          </td>
+
+          <td>
+            <input
+              type="number"
+              min="0"
+              max="1000"
+              id="bookNumberCopies"
+              name="numberCopies"
+              value={bookValue.numberCopies}
+              onChange={inputOnChange}
+              className={cn(styles.input, styles.inputNumber)}
+            />
+          </td>
+        </tr>
+      </>
+    );
+  }
 
   return (
     <div>
@@ -136,186 +382,14 @@ export default function BooksManagementPage(): JSX.Element {
         </MainHeaderWrapper>
 
         <div className={styles.flexBox}>
-          <table className={addBookTableClass()}>
+          <table className={addBookBlockTableClass()}>
             <tr className={cn(styles.inputBlock, styles.twoColumns)}>
               <td colSpan={2}>
                 Добавление книги
               </td>
             </tr>
 
-            <tr>
-              <td className={styles.inputTitle}>
-                <label htmlFor="bookTitle">
-                  Название книги
-                </label>
-              </td>
-
-              <td>
-                <input
-                  type="text"
-                  id="bookTitle"
-                  name="title"
-                  value={bookValue.title}
-                  onChange={inputOnChange}
-                  className={styles.input}
-                />
-              </td>
-            </tr>
-
-            <tr className={styles.inputBlock}>
-              <td className={styles.inputTitle}>
-                <label htmlFor="bookAuthor">
-                  ФИО автора
-                </label>
-              </td>
-
-              <td>
-                <input
-                  type="text"
-                  id="bookAuthor"
-                  name="author"
-                  value={bookValue.author}
-                  onChange={inputOnChange}
-                  className={styles.input}
-                />
-              </td>
-            </tr>
-
-            <tr className={styles.inputBlock}>
-              <td className={styles.inputTitle}>
-                <label htmlFor="bookPublishHouse">
-                  Издательство
-                </label>
-              </td>
-
-              <td>
-                <select
-                  id="bookPublishHouse"
-                  name="publishHouse"
-                  value={bookValue.publishHouse}
-                  onChange={inputOnChange}
-                  className={styles.input}
-                >
-                  <option selected />
-                  {lists.houses
-                  && lists.houses.map(({ title, id }) => {
-                    return (
-                      <option key={id + 1}>
-                        {title}
-                      </option>
-                    );
-                  })}
-                </select>
-              </td>
-            </tr>
-
-            <tr className={styles.inputBlock}>
-              <td className={styles.inputTitle}>
-                <label htmlFor="otherBookPublishHouse">
-                  Другое издательство
-                </label>
-              </td>
-
-              <td>
-                <input
-                  type="text"
-                  id="otherBookPublishHouse"
-                  name="otherPublishHouse"
-                  value={bookValue.otherPublishHouse}
-                  onChange={inputOnChange}
-                  className={styles.input}
-                />
-              </td>
-            </tr>
-
-            <tr className={styles.inputBlock}>
-              <td className={styles.inputTitle}>
-                <label htmlFor="bookGenre">
-                  Жанр
-                </label>
-              </td>
-
-              <td>
-                <select
-                  id="bookGenre"
-                  name="genre"
-                  value={bookValue.genre}
-                  onChange={inputOnChange}
-                  className={styles.input}
-                >
-                  <option selected />
-                  {lists.genres
-                  && lists.genres.map(({ title, id }) => {
-                    return (
-                      <option key={id + 1}>
-                        {title}
-                      </option>
-                    );
-                  })}
-                </select>
-              </td>
-            </tr>
-
-            <tr className={styles.inputBlock}>
-              <td className={styles.inputTitle}>
-                <label htmlFor="otherBookGenre">
-                  Другой жанр
-                </label>
-              </td>
-
-              <td>
-                <input
-                  type="text"
-                  id="otherBookGenre"
-                  name="otherGenre"
-                  value={bookValue.otherGenre}
-                  onChange={inputOnChange}
-                  className={styles.input}
-                />
-              </td>
-            </tr>
-
-            <tr className={styles.inputBlock}>
-              <td className={styles.inputTitle}>
-                <label htmlFor="bookPublishYear">
-                  Год издания
-                </label>
-              </td>
-
-              <td>
-                <input
-                  type="number"
-                  min="1800"
-                  max="2021"
-                  id="bookPublishYear"
-                  name="publishYear"
-                  value={bookValue.publishYear}
-                  onChange={inputOnChange}
-                  className={cn(styles.input, styles.inputNumber)}
-                />
-              </td>
-            </tr>
-
-            <tr className={styles.inputBlock}>
-              <td className={styles.inputTitle}>
-                <label htmlFor="bookNumberCopies">
-                  Количество экземпляров
-                </label>
-              </td>
-
-              <td>
-                <input
-                  type="number"
-                  min="0"
-                  max="1000"
-                  id="bookNumberCopies"
-                  name="numberCopies"
-                  value={bookValue.numberCopies}
-                  onChange={inputOnChange}
-                  className={cn(styles.input, styles.inputNumber)}
-                />
-              </td>
-            </tr>
+            {inputFieldsForAddAndEdit()}
 
             <tr className={cn(styles.inputBlock, styles.twoColumns)}>
               <td colSpan={2}>
@@ -331,9 +405,47 @@ export default function BooksManagementPage(): JSX.Element {
           </table>
 
           <table className={editBookTableClass()}>
+            <tr>
+              <td colSpan={2}>
+                <button
+                  type="button"
+                  onClick={goBack}
+                  className={styles.buttonImage}
+                >
+                  <Image
+                    src={GoBackIcon}
+                    width={30}
+                    height={30}
+                    alt="Вернуться назад"
+                  />
+                </button>
+              </td>
+            </tr>
             <tr className={cn(styles.inputBlock, styles.twoColumns)}>
               <td colSpan={2}>
                 Редактирование книги
+              </td>
+            </tr>
+
+            {inputFieldsForAddAndEdit()}
+
+            <tr className={cn(styles.inputBlock, styles.twoColumns)}>
+              <td colSpan={2}>
+                <button
+                  type="button"
+                  // onClick={handleEditBookSubmit}
+                  className={styles.button}
+                >
+                  Редактировать
+                </button>
+              </td>
+            </tr>
+          </table>
+
+          <table className={editBookBlockTableClass()}>
+            <tr className={cn(styles.inputBlock, styles.twoColumns)}>
+              <td colSpan={2}>
+                Редактирование / удаление книги
               </td>
             </tr>
 
@@ -371,14 +483,16 @@ export default function BooksManagementPage(): JSX.Element {
 
             <tr>
               <td colSpan={2} className={styles.booksContainer}>
-                {!(books.length === 0) ? (
+                {!(books.length === 0 && searchBookClick) ? (
                   books &&
                   books.map(({
-                     author,
-                     genreTitle,
-                     houseTitle,
-                     title,
-                     year
+                   id,
+                   author,
+                   genreTitle,
+                   houseTitle,
+                   title,
+                   year,
+                   numberCopies
                    }, index) => {
                     return (
                       <div
@@ -394,8 +508,12 @@ export default function BooksManagementPage(): JSX.Element {
                         <div className={styles.buttonContainer}>
                           <button
                             type="button"
-                            // onClick={}
-                            className={styles.buttonEditAndDelete}
+                            onClick={() => {
+                              setEditBookClick(true);
+                              setAddBookLinkClick(false);
+                              setEditBookLinkClick(false);
+                              editBook(title, author, genreTitle, houseTitle, year, numberCopies);}}
+                            className={styles.buttonImage}
                           >
                             <Image
                               src={EditBookIcon}
@@ -407,8 +525,8 @@ export default function BooksManagementPage(): JSX.Element {
 
                           <button
                             type="button"
-                            // onClick={}
-                            className={styles.buttonEditAndDelete}
+                            onClick={() => deleteBook(title, id)}
+                            className={styles.buttonImage}
                           >
                             <Image
                               src={DeleteBookIcon}
@@ -431,17 +549,25 @@ export default function BooksManagementPage(): JSX.Element {
             </tr>
           </table>
 
-          <div>
+          <div className={hideManagementBookMenuClass()}>
             <div
               className={activeAddBookClass()}
-              onClick={() => { setAddBookClick(true); setEditBookClick(false); }}
+              onClick={() => {
+                setAddBookLinkClick(true);
+                setEditBookLinkClick(false);
+                setEditBookClick(false);
+              }}
             >
               Добавление книги
             </div>
 
             <div
               className={activeEditBookClass()}
-              onClick={() => { setAddBookClick(false); setEditBookClick(true); }}
+              onClick={() => {
+                setAddBookLinkClick(false);
+                setEditBookLinkClick(true);
+                setEditBookClick(false);
+              }}
             >
               Редактирование книги
             </div>
